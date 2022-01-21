53ed6d7173fcdcd404123c082d4cfb9da782c3e8
rpw199912j <rpw199912j@gmail.com>
2022-01-21 00:06:26 -0600

    created miscibility gap plotting v1

--- thermodynamics/miscibility_gap.py
+++ thermodynamics/miscibility_gap.py
@@ -1,31 +1,173 @@
 import numpy as np
 from manim import *
+from scipy.optimize import fsolve
 
 
-class GibbsFreeEnergy(Scene):
+def common_tangent(x: list, omega, temperature, gas_constant=8.31):
+    return [((
+                     omega * x[1] * (1 - x[1]) + gas_constant * temperature * (x[1] * np.log(x[1]) +
+                                                                               (1 - x[1]) * np.log(1 - x[1]))
+             ) - (
+                     omega * x[0] * (1 - x[0]) + gas_constant * temperature * (x[0] * np.log(x[0]) +
+                                                                               (1 - x[0]) * np.log(1 - x[0]))
+             )) / (x[1] - x[0]) - (
+                    omega * (1 - 2 * x[1]) + gas_constant * temperature * (np.log(x[1]) - np.log(1 - x[1]))
+            ),
+            (
+                    omega * (1 - 2 * x[1]) + gas_constant * temperature * (np.log(x[1]) - np.log(1 - x[1]))
+            ) - (
+                    omega * (1 - 2 * x[0]) + gas_constant * temperature * (np.log(x[0]) - np.log(1 - x[0]))
+            )
+            ]
+
+
+def get_y_coord_on_gibbs(x: float, omega: float, temperature: float, gas_constant: float = 8.31) -> float:
+    return omega * x * (1 - x) + gas_constant * temperature * (x * np.log(x) +
+                                                               (1 - x) * np.log(1 - x))
+
+
+class MiscibilityGap(Scene):
     def construct(self):
-        # define the plotting axis
-        axes = Axes(
+        # define the plotting axis for Gibbs free energy vs. composition
+        axes_gibbs = Axes(
             x_range=[0, 1, 0.1],
-            y_range=[-1000, 500, 100],
+            y_range=[-600, 300, 100],
             x_length=6,
-            y_length=8,
+            y_length=3,
             tips=False
+        ).shift(UP * 2)
+
+        gibbs_y_label = axes_gibbs.get_y_axis_label(
+            Tex("$G$"), edge=LEFT, direction=LEFT, buff=0.4
         )
 
-        self.add(axes)
+        # define the plotting axis for temperature vs. composition
+        axes_temperature = Axes(
+            x_range=[0, 1, 0.1],
+            y_range=[180, 320, 20],
+            x_length=6,
+            y_length=3.5,
+            tips=False
+        ).shift(DOWN * 2)
 
-        # define the Gibbs Free energy as a function of temperature
-        temperature = ValueTracker(360)
-        omega = ValueTracker(5200)
+        temperature_y_label = axes_temperature.get_y_axis_label(
+            Tex("$T$"), edge=LEFT, direction=LEFT, buff=0.4
+        )
+        composition_label = axes_temperature.get_x_axis_label(Tex("$X$"))
+
+        self.add(axes_gibbs, axes_temperature)
+        self.play(
+            LaggedStart(
+                *[Write(label) for label in [temperature_y_label, composition_label, gibbs_y_label]],
+                lag_ratio=1
+            )
+        )
+        self.wait()
+
+        # define the Gibbs free energy as a function of composition at a given temperature and omega
+        gas_constant = 8.31
+        temperature_tracker = ValueTracker(190)
+        omega_tracker = ValueTracker(5200)
 
         gibbs_curve = always_redraw(
             lambda:
-            axes.plot(
-                lambda x: omega.get_value() * x * (1 - x) + 8.31 * temperature.get_value() * (
-                            x * np.log(x) + (1 - x) * np.log(1 - x)),
-                x_range=[1E-6, 1-1E-6]
+            axes_gibbs.plot(
+                lambda x: omega_tracker.get_value() * x * (1 - x) + gas_constant * temperature_tracker.get_value() * (
+                        x * np.log(x) + (1 - x) * np.log(1 - x)),
+                x_range=[1E-6, 1 - 1E-6]
             )
+        )
+
+        common_tangent_1 = always_redraw(
+            lambda:
+            Dot(point=axes_gibbs.c2p(
+                fsolve(common_tangent,
+                       np.array([0.1, 0.9]),
+                       args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                       )[0],
+                get_y_coord_on_gibbs(
+                    fsolve(common_tangent,
+                           np.array([0.1, 0.9]),
+                           args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                           )[0],
+                    omega_tracker.get_value(),
+                    temperature_tracker.get_value()
+                )
+            ), color=RED)
+        )
+
+        common_tangent_2 = always_redraw(
+            lambda:
+            Dot(point=axes_gibbs.c2p(
+                fsolve(common_tangent,
+                       np.array([0.1, 0.9]),
+                       args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                       )[1],
+                get_y_coord_on_gibbs(
+                    fsolve(common_tangent,
+                           np.array([0.1, 0.9]),
+                           args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                           )[1],
+                    omega_tracker.get_value(),
+                    temperature_tracker.get_value()
+                )
+            ), color=RED)
+        )
+
+        common_tangent_trace_1 = always_redraw(
+            lambda:
+            Dot(
+                point=axes_temperature.c2p(
+                    fsolve(common_tangent,
+                           np.array([0.1, 0.9]),
+                           args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                           )[0],
+                    temperature_tracker.get_value()
+                ),
+                color=BLUE
+            )
+        )
+
+        common_tangent_trace_2 = always_redraw(
+            lambda:
+            Dot(
+                point=axes_temperature.c2p(
+                    fsolve(common_tangent,
+                           np.array([0.1, 0.9]),
+                           args=(omega_tracker.get_value(), temperature_tracker.get_value())
+                           )[1],
+                    temperature_tracker.get_value()
+                ),
+                color=BLUE
+            )
+        )
+
+        trace_1_path = TracedPath(common_tangent_trace_1.get_center, stroke_color=BLUE, stroke_width=4)
+        trace_2_path = TracedPath(common_tangent_trace_2.get_center, stroke_color=BLUE, stroke_width=4)
+
+        hline_trace_1_to_2 = always_redraw(
+            lambda:
+            DashedLine(
+                start=common_tangent_1.get_center(),
+                end=common_tangent_2.get_center(),
+                color=RED
+            )
+        )
+
+        vline_trace_1 = always_redraw(
+            lambda:
+            DashedLine(
+                start=common_tangent_1.get_center(),
+                end=common_tangent_trace_1.get_center()
+            ).set_color_by_gradient(RED, BLUE)
+        )
+
+        vline_trace_2 = always_redraw(
+            lambda:
+            DashedLine(
+                start=common_tangent_2.get_center(),
+                end=common_tangent_trace_2.get_center()
+            ).set_color_by_gradient(RED, BLUE)
         )
 
         self.play(
@@ -34,6 +176,24 @@ class GibbsFreeEnergy(Scene):
         self.wait()
 
         self.play(
-            temperature.animate.set_value(0), run_time=4, rate_func=linear
+            Create(hline_trace_1_to_2)
+        )
+
+        self.play(
+            FadeIn(common_tangent_1, common_tangent_2)
+        )
+        self.wait()
+
+        self.play(
+            Create(vline_trace_1), Create(vline_trace_2)
+        )
+        self.play(
+            FadeIn(common_tangent_trace_1, common_tangent_trace_2)
+        )
+        self.add(trace_1_path, trace_2_path)
+        self.wait()
+
+        self.play(
+            temperature_tracker.animate.set_value(312.9), run_time=3, rate_func=linear
         )
         self.wait()
