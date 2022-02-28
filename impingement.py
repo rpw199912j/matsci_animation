61b0dc5ef57a1eca79257331f8a35b7b4d894f64
rpw199912j <rpw199912j@gmail.com>
2022-02-28 00:02:06 -0600

    tested visualizing horizontal slices

--- impingement.py
+++ impingement.py
@@ -70,3 +70,53 @@ class TwoCircleImpingement(Scene):
         #           circ_2_radius_tracker.animate(run_time=2, rate_func=linear).set_value(3))
         self.play(time_tracker.animate(run_time=2, rate_func=linear).set_value(2))
         self.wait()
+
+
+class TestHorizontalSlice(ThreeDScene):
+    def construct(self):
+        axes = ThreeDAxes(
+        )
+        # make the axes completely transparent
+        axes.set_opacity(opacity=0)
+
+        self.set_camera_orientation(70 * DEGREES, -20 * DEGREES)
+        self.add(axes)
+
+        unit_y_length = axes.c2p(0, 1, 0)[1] - axes.c2p(0, 0, 0)[1]
+        unit_y_vector = axes.c2p(0, 1, 0) - axes.c2p(0, 0, 0)
+        unit_z_vector = axes.c2p(0, 0, 1) - axes.c2p(0, 0, 0)
+
+        t_tracker = ValueTracker(0)
+        plane = always_redraw(
+            lambda:
+            Square(side_length=10 * unit_y_length).shift(
+                t_tracker.get_value() * unit_z_vector
+            ).set_opacity(0.3).set_stroke(opacity=0)
+        )
+        circ_1 = always_redraw(
+            lambda:
+            Circle(radius=2 * unit_y_length - t_tracker.get_value(), color=BLUE).shift(
+                t_tracker.get_value() * unit_z_vector
+            )
+        )
+        circ_2 = always_redraw(
+            lambda:
+            Circle(radius=(t_tracker.get_value() - 0.5) * 0.5, color=GREEN).shift(
+                0.5 * unit_y_vector + t_tracker.get_value() * unit_z_vector
+            ) if t_tracker.get_value() > 0.5 else Circle(radius=0)
+        )
+        circ_intersection = always_redraw(
+            lambda:
+            Intersection(circ_1, circ_2, color=YELLOW, fill_color=PURPLE, fill_opacity=1).shift(
+                t_tracker.get_value() * unit_z_vector
+            )
+            # if t_tracker.get_value() >= 0.5 else VMobject()
+        )
+
+        self.add(circ_1, circ_2, circ_intersection, plane)
+        self.play(
+            t_tracker.animate.set_value(3),
+            rate_func=linear,
+            run_time=3
+        )
+        self.wait()
