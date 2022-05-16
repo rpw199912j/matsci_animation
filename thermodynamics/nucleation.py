852f22db8714fa3927c001e7ef8040199202d788
rpw199912j <rpw199912j@gmail.com>
2022-05-16 11:43:36 -0500

    added the first iteration of the critical radius animation

---
+++ thermodynamics/nucleation.py
@@ -0,0 +1,152 @@
+import numpy as np
+from manim import *
+
+
+class CriticalRadius(Scene):
+    def construct(self):
+        # define the plotting axes
+        x_max = 1.2
+        y_max = 1
+        axes = Axes(
+            x_range=[0, x_max, 0.2],
+            y_range=[-y_max, y_max, 0.5],
+            x_length=8,
+            y_length=6,
+            tips=False
+        )
+
+        x_label = axes.get_x_axis_label(Tex("$r$"))
+        y_label = axes.get_y_axis_label(Tex("$\Delta G$"))
+
+        # define the variables
+        num_of_lines = 100
+        delta_G = -0.18
+        interfacial_energy = 0.054
+        r_crit = 2 * interfacial_energy / np.abs(delta_G)
+        y_shift = 0.035
+
+        # define the volume Gibbs free energy term
+        volume_G = axes.plot(
+            lambda r:
+            4 / 3 * np.pi * r ** 3 * delta_G,
+            x_range=(0, x_max), color=RED
+        )
+
+        # define the surface Gibbs free energy term
+        surface_G = axes.plot(
+            lambda r:
+            4 * np.pi * r ** 2 * interfacial_energy,
+            x_range=(0, x_max), color=GREEN
+        )
+
+        # define the total energy
+        total_G = axes.plot(
+            lambda r:
+            4 / 3 * np.pi * r ** 3 * delta_G + 4 * np.pi * r ** 2 * interfacial_energy,
+            x_range=(0, x_max), color=YELLOW
+        )
+
+        total_G_shifted_left = axes.plot(
+            lambda r:
+            4 / 3 * np.pi * r ** 3 * delta_G + 4 * np.pi * r ** 2 * interfacial_energy + y_shift,
+            x_range=(0, r_crit)
+        )
+        total_G_shifted_right = axes.plot(
+            lambda r:
+            4 / 3 * np.pi * r ** 3 * delta_G + 4 * np.pi * r ** 2 * interfacial_energy + y_shift,
+            x_range=(r_crit, x_max)
+        )
+
+        self.play(
+            Create(axes)
+        )
+        self.play(
+            Write(x_label),
+            Write(y_label)
+        )
+        self.wait()
+
+        self.play(
+            Create(volume_G)
+        )
+        self.wait()
+
+        self.play(
+            Create(surface_G)
+        )
+        self.wait()
+
+        # create the vertical line
+        volume_G_lines = axes.get_vertical_lines_to_graph(
+            volume_G, x_range=[0, x_max], color=BLUE,
+            num_lines=num_of_lines, line_config={"dashed_ratio": 0.9}
+        )
+        self.play(
+            Create(volume_G_lines), run_time=2
+        )
+        self.wait()
+
+        shift_vectors = [
+            axes.c2p(
+                x_coord, 4 * np.pi * x_coord ** 2 * interfacial_energy
+            ) - axes.c2p(
+                x_coord, 0
+            )
+            for x_coord in np.linspace(0, x_max, num_of_lines)
+        ]
+
+        # shift all the lines in sin_curve_lines
+        self.play(
+            LaggedStart(
+                *[vline.animate.shift(shift_vector)
+                  for vline, shift_vector in zip(volume_G_lines, shift_vectors)],
+                lag_ratio=0.3
+            ),
+            run_time=3
+        )
+        self.wait()
+
+        # morph the surface_G line to the total_G line
+        self.play(
+            TransformFromCopy(surface_G, total_G)
+        )
+        self.wait()
+
+        # remove the surface_G, volume_G and vertical lines
+        self.play(
+            FadeOut(surface_G, volume_G, *volume_G_lines)
+        )
+        self.wait()
+
+        # add a circle
+        dot = Dot(
+            point=total_G_shifted_left.get_end()
+        )
+        self.play(
+            DrawBorderThenFill(dot)
+        )
+        self.wait()
+
+        total_G_shifted_left.reverse_points()
+        # self.add(total_G_shifted_left)
+
+        self.play(
+            MoveAlongPath(dot, total_G_shifted_left),
+            run_time=2
+        )
+        self.wait()
+
+        total_G_shifted_left.reverse_points()
+        self.play(
+            MoveAlongPath(dot, total_G_shifted_left),
+            run_time=2
+        )
+        self.wait()
+
+        # self.add(total_G_shifted_right)
+        self.play(
+            MoveAlongPath(dot, total_G_shifted_right),
+            run_time=2
+        )
+        self.wait()
+
