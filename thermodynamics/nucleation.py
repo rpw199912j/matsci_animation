0b9f3d267a0cc46767ab28b3b37469ef51c5c12f
rpw199912j <rpw199912j@gmail.com>
2022-05-17 14:14:24 -0500

    added the first iteration of the changing variable animation

--- thermodynamics/nucleation.py
+++ thermodynamics/nucleation.py
@@ -217,3 +217,138 @@ class CriticalRadius(Scene):
             run_time=2
         )
         self.wait()
+
+
+# noinspection DuplicatedCode
+class ChangeVariables(Scene):
+    @staticmethod
+    def get_r_crit(interfacial_energy, delta_G):
+        r_crit = 2 * interfacial_energy / np.abs(delta_G)
+        return r_crit
+
+    @staticmethod
+    def get_delta_G_crit(interfacial_energy, delta_G):
+        r_crit = 2 * interfacial_energy / np.abs(delta_G)
+        delta_G_crit = 4 / 3 * np.pi * r_crit ** 3 * delta_G + (
+                4 * np.pi * r_crit ** 2 * interfacial_energy
+        )
+        return delta_G_crit
+
+    def construct(self):
+        # define the plotting axes
+        x_max = 1.2
+        y_max = 1
+        axes = Axes(
+            x_range=[0, x_max, 0.2],
+            y_range=[-y_max, y_max, 0.5],
+            x_length=8,
+            y_length=6,
+            tips=False
+        ).shift(LEFT * 2)
+
+        # get the axes labels
+        x_label = axes.get_x_axis_label(Tex("$r$"))
+        y_label = axes.get_y_axis_label(Tex("$\Delta G$"))
+
+        # define the variables
+        delta_G_tracker = ValueTracker(-0.44)
+        interfacial_energy_tracker = ValueTracker(0.16)
+
+        # define the volume Gibbs free energy term
+        volume_G = always_redraw(
+            lambda:
+            axes.plot(
+                lambda r:
+                4 / 3 * np.pi * r ** 3 * delta_G_tracker.get_value(),
+                x_range=(0, x_max), color=RED
+            )
+        )
+
+        # define the surface Gibbs free energy term
+        surface_G = always_redraw(
+            lambda:
+            axes.plot(
+                lambda r:
+                4 * np.pi * r ** 2 * interfacial_energy_tracker.get_value(),
+                x_range=(0, x_max), color=GREEN
+            )
+        )
+
+        # define the total energy
+        total_G = always_redraw(
+            lambda:
+            axes.plot(
+                lambda r:
+                4 / 3 * np.pi * r ** 3 * delta_G_tracker.get_value() + (
+                        4 * np.pi * r ** 2 * interfacial_energy_tracker.get_value()
+                ),
+                x_range=(0, x_max), color=YELLOW
+            )
+        )
+
+        # highlight the r_crit and G_crit
+        lines_crit_1 = always_redraw(
+            lambda:
+            DashedLine(
+                start=axes.c2p(self.get_r_crit(interfacial_energy_tracker.get_value(),
+                                               delta_G_tracker.get_value()),
+                               self.get_delta_G_crit(
+                                   interfacial_energy_tracker.get_value(),
+                                   delta_G_tracker.get_value()
+                               )),
+                end=axes.c2p(0, self.get_delta_G_crit(
+                    interfacial_energy_tracker.get_value(),
+                    delta_G_tracker.get_value()
+                ))
+            )
+        )
+
+        lines_crit_2 = always_redraw(
+            lambda:
+            DashedLine(
+                start=axes.c2p(self.get_r_crit(interfacial_energy_tracker.get_value(),
+                                               delta_G_tracker.get_value()),
+                               self.get_delta_G_crit(
+                                   interfacial_energy_tracker.get_value(),
+                                   delta_G_tracker.get_value()
+                               )),
+                end=axes.c2p(self.get_r_crit(interfacial_energy_tracker.get_value(),
+                                             delta_G_tracker.get_value()),
+                             0),
+            )
+        )
+
+        # add the critical radius and Delta G label
+        delta_G_crit_label = always_redraw(
+            lambda:
+            Tex(
+                r"$\Delta G^*$"
+            ).next_to(lines_crit_1.get_left(), direction=LEFT, buff=0.1)
+        )
+
+        r_crit_label = always_redraw(
+            lambda:
+            Tex(
+                r"$r^*$"
+            ).next_to(lines_crit_2.get_bottom(), direction=DOWN, buff=0.1)
+        )
+
+        self.add(
+            axes, x_label, y_label, volume_G, surface_G, total_G,
+            lines_crit_1, lines_crit_2, r_crit_label, delta_G_crit_label
+        )
+        self.wait()
+
+        # increase the magnitude of delta_G
+        self.play(
+            delta_G_tracker.animate.set_value(-0.7),
+            run_time=2
+        )
+        self.wait()
+
+        # increase the magnitude of interfacial_energy
+        self.play(
+            interfacial_energy_tracker.animate.set_value(0.25),
+            run_time=2
+        )
+        self.wait()
