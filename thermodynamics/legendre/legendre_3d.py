a2c60b6956f7bcbb6253d8eab634bded15ae8fdb
rpw199912j <rpw199912j@gmail.com>
2023-02-17 17:24:29 -0800

    deleted exploratory codes

--- thermodynamics/legendre/legendre_3d.py
+++ thermodynamics/legendre/legendre_3d.py
@@ -1,45 +1,26 @@
 import numpy as np
-import plotly.graph_objects as go
 import sympy as smp
 
-# a = 0.01
-# x = np.linspace(0, 100, num=101)
-# s = 2 * a * x
-# X, S = np.meshgrid(x, s)
-# U = a * np.power(X, 2)
-# G = U - X * S
-#
-# fig = go.Figure(data=[go.Surface(z=G, x=x, y=s)])
-# fig.show()
+from manim import *
 
+# Define some constants
 a = 0.01
 b = 0.005
 c = 150
-# s = np.linspace(0, 100, num=101)
-# v = np.linspace(0, c, num=101)
-# S, V = np.meshgrid(s, v)
-# U = a * np.power(S, 2) + b * np.power(V - c, 2)
-# G = U - X * S
 
+# Define thermodynamic variables and potentials
 s, v = smp.symbols("s v", real=True)
+# symbolic U(S,V)
 u = a * s ** 2 + b * (v - c) ** 2
+# use u_f internal as a function that takes entropy and v as arguments
 u_f = smp.lambdify(args=[s, v], expr=u)
+# get the partial derivatives and their physical variables
 duds = smp.diff(u, s)
 t = duds
 dudv = smp.diff(u, v)
 p = - dudv
+# use legendre transform to obtain G(T,P)
 g = u - t * s + p * v
 
 u_s = u.subs(v, 10)
 u_v = u.subs(s, 10)
-print(g)
-
-# fig = go.Figure(data=[go.Surface(z=U, x=s, y=v)])
-# fig.update_layout(
-#     scene=dict(
-#         xaxis_title="S",
-#         yaxis_title="V",
-#         zaxis_title="U"
-#     )
-# )
-# fig.show()
